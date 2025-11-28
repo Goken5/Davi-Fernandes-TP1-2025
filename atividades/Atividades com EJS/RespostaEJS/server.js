b610ac53054869ab08954264bcf62159a1327e82
Goken5 <fdavipessanha@gmail.com>
2025-11-28 14:08:31 -0300

    atividade

--- atividades/Atividades com EJS/RespostaEJS/server.js
+++ atividades/Atividades com EJS/RespostaEJS/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/index", (request, response) => {
+app.get("/", (request, response) => {
     response.render("index");
 });
 
